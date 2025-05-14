1ae51a75157444e5e60894eddac2a61078006fee
gabimmdev <gabrielagomescezar@gmail.com>
2025-05-14 11:51:15 -0300

    feat: return AppNavigator in App

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppNavigator from './app/navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <View>
-    </View>
-  );
+  return <AppNavigator />;
 }
+
